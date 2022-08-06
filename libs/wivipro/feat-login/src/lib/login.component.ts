f5ac400c9155c66d8eabc04eae27031e26e484a1
Tim Geerts <tim.geerts1@gmail.com>
2022-08-06 17:25:18 +0200

    better form stuff in long

--- libs/wivipro/feat-login/src/lib/login.component.ts
+++ libs/wivipro/feat-login/src/lib/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, NgZone } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '@geerts/shared';
 
 @Component({
@@ -7,18 +13,41 @@ import { AuthService } from '@geerts/shared';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor(private authService: AuthService, private ngZone: NgZone) {}
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private ngZone: NgZone
+  ) {}
+
+  form = this.fb.group({
+    username: ['fake@fake.fake', Validators.required],
+    password: ['xxx', Validators.required],
+    remember: [true],
+  });
+
+  onSubmit(): void {
+    console.log(this.form);
+  }
 
   login(): void {
-    this.authService
-      .signIn('tim.geerts1@gmail.com', 'Linneke1')
-      .then((result) => {
-        this.ngZone.run(() => {
-          // this.router.navigate(['dashboard']);
+    const username = this.form.get('username')?.value;
+    const password = this.form.get('password')?.value;
+    const remember = this.form.get('remember')?.value;
+
+    try {
+      this.authService
+        .signIn(username, password)
+        .then((result) => {
+          console.log(result);
+          this.ngZone.run(() => {
+            // this.router.navigate(['dashboard']);
+          });
+        })
+        .catch((error) => {
+          console.error('test', error.code);
         });
-      })
-      .catch((error) => {
-        console.error('test', error.message);
-      });
+    } catch (error) {
+      console.log('all good');
+    }
   }
 }
